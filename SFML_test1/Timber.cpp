1c09c85d2d824e954c85691b22a41db6450869b3
bbbaipooo <baipoo_B@hotmail.com>
2021-10-05 23:48:55 +0700

    background

--- SFML_test1/Timber.cpp
+++ SFML_test1/Timber.cpp
@@ -1,8 +1,9 @@
 ﻿//important libraries in C++
 #include"stdafx.h"//" " is for file included with Visual Studio
 #include<SFML/Graphics.hpp>//include SFML graphics from the folder
+#include<iostream>
 
-//using namespace for make code easier
+// when create class จะถูกเก็บในนี้ //using namespace for make code easier
 using namespace sf;
 
 int main()
@@ -12,6 +13,51 @@ int main()
 
 	// create & show a window for the game
 	RenderWindow window(size, "DwarfCutTheTree!!!", Style::Fullscreen);
+
+	////// Texture
+	Texture backgroundTexture;
+	if (!backgroundTexture.loadFromFile("graphics/Background2.jpg"))
+	{
+		std::cout << "Load Failed";
+	}
+	
+	////// Sprite
+	Sprite backgroundSprite;
+	backgroundSprite.setTexture(backgroundTexture);
+	backgroundSprite.setPosition(0, 0);//set to cover the screen
+
+	while (window.isOpen()) 
+	{
+		/*
+		*********************************************
+		          Handle the players input
+		*********************************************
+		*/
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		/*
+		*********************************************
+		              Update the scene
+		*********************************************
+		*/
+		/*
+		*********************************************
+		               Draw the scene
+		*********************************************
+		*/
+		// Clear everything from the last frame
+		window.clear();
+
+		// Draw game scene here
+		window.draw(backgroundSprite);
+
+		// Show everything I just draw
+		window.display();
+
+	}
+
 
 	return 0;
 }
