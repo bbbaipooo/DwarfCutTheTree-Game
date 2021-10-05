6d47aa0017f9a900b59cb668d8cb45d4bb765890
bbbaipooo <baipoo_B@hotmail.com>
2021-10-05 17:44:06 +0700

    Add project files.

---
+++ SFML_test1/Timber.cpp
@@ -0,0 +1,17 @@
+﻿//important libraries in C++
+#include"stdafx.h"//" " is for file included with Visual Studio
+#include<SFML/Graphics.hpp>//include SFML graphics from the folder
+
+//using namespace for make code easier
+using namespace sf;
+
+int main()
+{
+	// create video mode
+	VideoMode size(1920, 1080);
+
+	// create & show a window for the game
+	RenderWindow window(size, "DwarfCutTheTree!!!", Style::Fullscreen);
+
+	return 0;
+}
